442fc2b3bb02ca96d529f17f4019be45612d0604
victorgis <essangvictor@gmail.com>
2022-03-08 17:24:25 +0100

    corrections

--- mail.php
+++ mail.php
@@ -20,37 +20,5 @@
   $headers .= "Reply-To: $visitor_email \r\n";
 
   mail($to,$email_subject,$email_body,$headers);
-
-  function IsInjected($str)
-    {
-        $injections = array('(\n+)',
-            '(\r+)',
-            '(\t+)',
-            '(%0A+)',
-            '(%0D+)',
-            '(%08+)',
-            '(%09+)'
-            );
-                
-        $inject = join('|', $injections);
-        $inject = "/$inject/i";
-        
-        if(preg_match($inject,$str))
-        {
-        return true;
-        }
-        else
-        {
-        return false;
-        }
-    }
-
-    if(IsInjected($visitor_email))
-    {
-        echo "Bad email value!";
-        exit;
-    }
-
-
-
+  
 ?>
